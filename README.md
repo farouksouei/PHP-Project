5f42b2eada60892cceec68b5bff27e615b22cfbe
farouk <farouksouei@gmail.com>
2022-05-21 23:19:09 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # PHP-Project
+# PHP-Project
---
+++ register.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/bootstrap.min.css" />
+  <link rel="stylesheet" href="icon-font/lineicons.css">
+  <title>Registration</title>
+</head>
+
+<body>
+  <?php include_once 'templates/navbar.php' ?>
+</body>
+<script src="js/bootstrap.min.js"></script>
+
+</html>
